3c763801ffc5ee0221101ef4da7b7eebd4dfc870
mrArvik <aristarhov.arvold@gmail.com>
2023-03-22 15:36:43 +0200

    I have done convertor but with no GUI, got some new ideas

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 # try to use tkinter, (pdfplumber, PyPDF2 - > mb without it)
 # should use gtts, tika (from tika import parser)
-#signed in a new computer
-from tkinter import *
-from tkinter import ttk
+# signed in a new computer
+# should try pyqt
 
+from tkinter import *
+from tkinter import ttk, filedialog
+from tkinter.filedialog import askopenfile
+import gtts
+from tika import parser
+import codecs
 
 
 window = Tk()
+
+window.title("PDF to mp3 converter")
 
 window.geometry('600x400')
 window.resizable(True, True)
 window.configure(background="#B53E3E")
 window.attributes('-alpha',0.9)
 
-window.title("PDF to mp3 converter")
+
+pdf_file = parser.from_file('225010558.pdf')
+tts = gtts.gTTS(pdf_file['content'], lang='ru')
+tts.save('audiotest1.mp3')
+
+
 
 
 
