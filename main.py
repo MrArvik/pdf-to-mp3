a534e0c84bfa8b857ce2883c0ec09cf10b09f52d
MrArvik <aristarhov.arvold@gmail.com>
2023-03-24 11:38:43 +0200

    Tried to make get button

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 from tkinter import *
 from tkinter import ttk, filedialog
 from tkinter.filedialog import askopenfile
+from tkinter import filedialog as fd
 import gtts
 from tika import parser
 import codecs
@@ -15,16 +16,40 @@ import codecs
 window = Tk()
 
 window.title("PDF to mp3 converter")
-
 window.geometry('600x400')
 window.resizable(True, True)
 window.configure(background="#B53E3E")
 window.attributes('-alpha',0.9)
 
+def select_file():
+    filetypes = (
+        ('pdf files', '*.pdf'),
+        ('All files', '*.*')
+    )
 
-pdf_file = parser.from_file('225010558.pdf')
-tts = gtts.gTTS(pdf_file['content'], lang='ru')
-tts.save('audiotest1.mp3')
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    fileinfo = fd.showinfo(
+        title='Selected File',
+        message=filename
+    )
+
+open_button = ttk.Button(
+    window,
+    text='Open a File',
+    command=select_file
+)
+
+open_button.pack(expand=True)
+
+
+
+#pdf_file = parser.from_file('225010558.pdf')
+#tts = gtts.gTTS(pdf_file['content'], lang='ru')
+#tts.save('audiotest1.mp3')
 
 
 
