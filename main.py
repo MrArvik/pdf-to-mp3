9530df00fa3b6d66fd09a5303cda14facbf7815b
MrArvik <aristarhov.arvold@gmail.com>
2023-04-14 13:45:20 +0300

    1st version

--- main.py
+++ main.py
@@ -4,14 +4,23 @@
 # should try pyqt
 #test
 
+
+
+
+# importing tkinter and tkinter.ttk
+# and all their functions and classes
 from tkinter import *
-from tkinter import ttk, filedialog
+from tkinter.ttk import *
+
+# importing askopenfile function
+# from class filedialog
 from tkinter.filedialog import askopenfile
-from tkinter import filedialog as fd
-from tkinter import messagebox
+
+
+import PyPDF2
 import gtts
 from tika import parser
-import codecs
+import os
 
 
 window = Tk()
@@ -22,35 +31,17 @@ window.resizable(True, True)
 window.configure(background="#B53E3E")
 window.attributes('-alpha',0.9)
 
-def select_file():
-    filetypes = (
-        ('pdf files', '*.pdf'),
-        ('All files', '*.*')
-    )
 
-    filename = fd.askopenfilename(
-        title='Open a file',
-        initialdir='/',
-        filetypes=filetypes)
+def open_file():
+    file = askopenfile(mode='rb', filetypes=[('PDF Files', '*.pdf')])
+    if file is not None:
+        content = file.name
+        print(content)
+        pdf_file = parser.from_file(str(content))
+        tts = gtts.gTTS(pdf_file['content'], lang='ru')
+        tts.save('audiotest1.mp3')
 
-    fileinfo = messagebox.showinfo(
-        title='Selected File',
-        message=filename
-    )
-
-open_button = ttk.Button(
-    window,
-    text='Open a File',
-    command=select_file
-)
-
-open_button.pack(expand=True)
-
-
-
-#pdf_file = parser.from_file(filename())
-#tts = gtts.gTTS(pdf_file['content'], lang='ru')
-#tts.save('audiotest1.mp3')
-
+btn = Button(window, text="Open", command=lambda: open_file())
+btn.pack(pady=20)
 
 window.mainloop()
